a9eaf1904c6a9852b316d70b0439a5ad895892e4
Yuri Ramos <yuri.pramos@gmail.com>
2017-04-04 18:41:30 -0300

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import SearchBar from "../containers/search_bar.js";
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      	<SearchBar/>
+      </div>
+    );
+  }
+}
